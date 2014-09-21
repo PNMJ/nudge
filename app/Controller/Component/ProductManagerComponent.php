7ffef97e5546c0d13d121973128aae4ee7539d27
Michael Ozeryansky <mozer624@gmail.com>
2014-09-20 20:42:16 -0400

    product stuff and spacing

--- app/Controller/Component/ProductManagerComponent.php
+++ app/Controller/Component/ProductManagerComponent.php
@@ -8,38 +8,43 @@ class ProductManagerComponent extends Component
 
     public function getProductsWithCategory($category = '')
     {
-        $url = "http://open.api.ebay.com/shopping?";
-        $maxents = 20;
-        $appid = self::APPID;
-
-        //nabbed from the tut
-        $endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';
-        $version = '1.0.0';
-        $globalid = 'EBAY-US';
-        $query = $category;
-        $safequery = urlencode($query);
-
-        $apicall = "$endpoint?";
-        $apicall .= "OPERATION-NAME=findItemsByKeywords";
-        $apicall .= "&SERVICE-VERSION=$version";
-        $apicall .= "&SECURITY-APPNAME=$appid";
-        $apicall .= "&GLOBAL-ID=$globalid";
-        $apicall .= "&keywords=$safequery";
-        $apicall .= "&paginationInput.entriesPerPage=$maxents";
-
-        $resp = simplexml_load_file($apicall);
-        $results = '';
-        $return = array();
-
-       foreach($resp->searchResult->item as $item) {
-            $pic   = $item->galleryURL;
-            $link  = $item->viewItemURL;
-            $title = $item->title;
-
-            $return[] = $item;
-            $results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td></tr>";
-        }
-        return $return;
-        }
+		$url = "http://open.api.ebay.com/shopping?";
+		$maxents = 20;
+		$appid = self::APPID;
+		
+		//nabbed from the tut
+		$endpoint = 'http://svcs.ebay.com/services/search/FindingService/v1';
+		$version = '1.0.0';
+		$globalid = 'EBAY-US';
+		$query = $category;
+		$safequery = urlencode($query);
+		
+		$apicall = "$endpoint?";
+		$apicall .= "OPERATION-NAME=findItemsByKeywords";
+		$apicall .= "&SERVICE-VERSION=$version";
+		$apicall .= "&SECURITY-APPNAME=$appid";
+		$apicall .= "&GLOBAL-ID=$globalid";
+		$apicall .= "&keywords=$safequery";
+		$apicall .= "&paginationInput.entriesPerPage=$maxents";
+		
+		$resp = simplexml_load_file($apicall);
+		$results = '';
+		$products = array();
+		
+		if(!is_array($resp->searchResult->item)){
+			return array();
+		}
+		
+		foreach($resp->searchResult->item as $item) {
+			$pic   = $item->galleryURL;
+			$link  = $item->viewItemURL;
+			$title = $item->title;
+			
+			$products[] = $item;
+			//$results .= "<tr><td><img src=\"$pic\"></td><td><a href=\"$link\">$title</a></td></tr>";
+		}
+		
+		return $products;
+	}
 
 }
